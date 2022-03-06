e6488d3cc3030b2d6849b53687dabc7fc7bf3be3
soozynn <81217456+soozynn@users.noreply.github.com>
2022-03-06 13:25:58 +0900

    [20] 몬스터 폭발 Motion (#7)
    * Feat: 몬스터 폭발 motion

--- src/audio.js
+++ src/audio.js
@@ -1,10 +1,11 @@
 import { Howl } from "howler";
-import audioMonsterSquash from "./audio/AudioMonsterSquash.mp3";
+import audioMonsterSquash from "./audio/audioMonsterSquash.mp3";
 import audioGameOver from "./audio/audioGameOver.mp3";
 import audioGameWin from "./audio/audioGameWin.mp3";
 import audioStage1Music from "./audio/audioBackground01.mp3";
 import audioFalling from "./audio/audioFalling.wav";
 import audioHurt from "./audio/audioHurt.wav";
+import audioJump from "./audio/audioJump.mp3";
 
 export const audio = {
 	gameOver: new Howl({
@@ -30,5 +31,9 @@ export const audio = {
 	hurt: new Howl({
 		src: [audioHurt],
 		volume: 0.5,
+	}),
+	jump: new Howl({
+		src: [audioJump],
+		volume: 0.2,
 	}),
 };
