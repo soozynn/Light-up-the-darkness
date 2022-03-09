030ebfce9704a3baff5bc2e888a4a1f49f37f5ea
soozynn <81217456+soozynn@users.noreply.github.com>
2022-03-09 20:05:29 +0900

    Fix: conflict

--- dist/main.js
+++ dist/main.js
@@ -249,14 +249,9 @@ eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpac
 				eval(
 					'__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)\n/* harmony export */ });\n/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (__webpack_require__.p + "jumpingLeft.png?663b1234c0e755a65a365cb109611088");\n\n//# sourceURL=webpack:///./src/img/player/jumpingLeft.png?',
 				);
-<<<<<<< HEAD
 
 "use strict";
 eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (__WEBPACK_DEFAULT_EXPORT__)\n/* harmony export */ });\n/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (__webpack_require__.p + \"jumpingLeft.png?663b1234c0e755a65a365cb109611088\");\n\n//# sourceURL=webpack:///./src/img/player/jumpingLeft.png?");
-=======
-				/***/
-			},
->>>>>>> ecb12b2 (Feat: 시작페이지)
 
 		/***/ "./src/img/player/jumpingRight.png":
 			/*!*****************************************!*\
