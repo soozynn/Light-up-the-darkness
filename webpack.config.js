9ac7783ea83f43dbc6753cbf46b09781809ce75d
soozynn <81217456+soozynn@users.noreply.github.com>
2022-03-16 15:55:17 +0900

    Fix: Change the index.html location

--- webpack.config.js
+++ webpack.config.js
@@ -50,7 +50,7 @@ module.exports = {
 	},
 	plugins: [
 		new HtmlWebpackPlugin({
-			template: path.resolve(__dirname, "./src/index.html"),
+			template: path.resolve(__dirname, "./src/public/index.html"),
 		}),
 		new CleanWebpackPlugin(),
 	],
